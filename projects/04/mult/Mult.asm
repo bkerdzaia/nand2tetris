a40bc57adb5f2df3a9513b92547c99de7d1ff461
Baduri Kerdzaia <bkerd14@freeuni.edu.ge>
2016-04-07 21:21:42 +0400

    Multiplies numbers

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,21 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+	@2
+	M=0	  // M[2] = 0 
+(LOOP)
+	@0 	  	
+	D=M		// D = M[0]
+	@END	
+	D;JLE
+	@1
+	D=M 	// D = M[1]
+	@2
+	M=D+M	// M[2] += D
+	@0 
+	M=M-1	// M[0]--
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
